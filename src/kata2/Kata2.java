f0551045147a81d56eccddc68182f3e8c4c5156b
usuario <usuario@10.22.143.55>
2016-09-30 11:24:00 +0100

    cuarta versión, model view controller

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,10 +7,8 @@ public class Kata2 {
        //Integer [] vector = {1,4,7,1,1,8,4,1,4,9};
        //Double [] vector = {1.1,1.2,1.1,2.2,3.2};
        String [] vector = {"Pedro","Pedro","Juan","Dani","Marco","Dani"};
-    
-       Histogram histo = new Histogram(vector);
        
-       HashMap<Object,Integer> histogram = histo.getHistogram();
+       Histogram<String> histogram = CalculateHistogram.computeHistogram(vector);
        
         for (Object key : histogram.keySet()) {
             System.out.println(key + "-->" + histogram.get(key));
