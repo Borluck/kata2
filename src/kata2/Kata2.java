8e2a5a6688ae85ae64a9fdaeee4eadb7e69317c4
usuario <usuario@10.22.143.55>
2016-09-30 10:51:34 +0100

    Tercera versión, clase Histogram genérica

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -4,14 +4,15 @@ import java.util.HashMap;
 
 public class Kata2 {
     public static void main (String[]args){
-       int [] vector = {1,4,7,1,1,8,4,1,4,9};
-       
+       //int [] vector = {1,4,7,1,1,8,4,1,4,9};
+       //Double [] vector = {1.1,1.2,1.1,2.2,3.2};
+       String [] vector = {"Pedro","Pedro","Juan","Dani","Marco","Dani"};
+    
        Histogram histo = new Histogram(vector);
        
-       HashMap<Integer,Integer> histogram = histo.getHistogram();
+       HashMap<Object,Integer> histogram = histo.getHistogram();
        
-       
-        for (int key : histogram.keySet()) {
+        for (Object key : histogram.keySet()) {
             System.out.println(key + "-->" + histogram.get(key));
         }
     }
