33720ca0f38c95bd3e6e263dc89768f0d6dc6d6e
usuario <usuario@10.22.143.55>
2016-09-30 10:55:26 +0100

    Tercera versión, clase Histogram genérica

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Kata2 {
     public static void main (String[]args){
-       //int [] vector = {1,4,7,1,1,8,4,1,4,9};
+       //Integer [] vector = {1,4,7,1,1,8,4,1,4,9};
        //Double [] vector = {1.1,1.2,1.1,2.2,3.2};
        String [] vector = {"Pedro","Pedro","Juan","Dani","Marco","Dani"};
     
